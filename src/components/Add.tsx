48061e353e906a3b4615b05fb3e74541ad44e0f2
Dzmitry Butenka <dima@butenko.by>
2023-05-16 10:56:50 -0700

    Build issues fixed

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,14 +1,9 @@
 import styles from "./Add.module.scss";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCirclePlus} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 function Add() {
     return (
         <div className={`card ${styles["add-card"]}`}>
-            {/*<button className={styles.button}>*/}
-            {/*    <FontAwesomeIcon icon={faCirclePlus} size="2xl" className={styles.icon}/>*/}
-            {/*</button>*/}
             <input
                 type="text"
                 className={styles.input}
